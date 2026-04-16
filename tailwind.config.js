18d9adce9f36c0a31057d43685b52a7ed1b017f4
matheusmoreiradebug <matheus189moreira@gmail.com>
2026-04-15 23:48:45 -0300

    fix: css build error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        brand: { DEFAULT: '#7B2FF7', light: '#9F5AFD', dark: '#5B1FBD' },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
